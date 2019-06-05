32e40095f404bd384803dbf27a293d93efb8a1bf
TobKed <tobiaszkedzierski@gmail.com>
2019-06-05 06:27:48 +0200

    add db and user model

--- resources.py
+++ resources.py
@@ -1,14 +1,32 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from models import UserModel
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('username', help='This field cannot be blank', required=True)
+parser.add_argument('password', help='This field cannot be blank', required=True)
 
 
 class UserRegistration(Resource):
     def post(self):
-        return {'message': 'User registration'}
+        data = parser.parse_args()
+        new_user = UserModel(
+            username=data['username'],
+            password=data['password']
+        )
+        try:
+            new_user.save_to_db()
+            return {
+                'message': f'User {new_user.username} was created'
+            }
+        except:
+            return {'message': 'Something went wrong'}, 500
 
 
 class UserLogin(Resource):
     def post(self):
-        return {'message': 'User login'}
+        data = parser.parse_args()
+        return data
 
 
 class UserLogoutAccess(Resource):
--- run.py
+++ run.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some-secret-string'
+
+
+db = SQLAlchemy(app)
 
 import views, models, resources
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 api.add_resource(resources.UserRegistration, '/registration')
